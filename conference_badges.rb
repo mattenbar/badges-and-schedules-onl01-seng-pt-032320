ff8a091f2b3b20f4f43aa31f17efad19fbb377ce
matthew enbar <matthew.enbar@gmail.com>
2020-03-24 18:45:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ end
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
-  new_array =  array.each{|x| "Hello, my name is #{x}."}
+  new_array =  speakers.each{|x| "Hello, my name is #{x}."}
   return new_array
 end
