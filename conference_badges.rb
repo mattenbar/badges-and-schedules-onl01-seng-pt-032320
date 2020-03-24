8acdd7307c19aef914e3fc1ec9aa96ab5f236820
matthew enbar <matthew.enbar@gmail.com>
2020-03-24 18:44:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
   new_array =  array.each{|x| "Hello, my name is #{x}."}
